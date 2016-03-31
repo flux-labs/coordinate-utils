4b702559f4b82cb845d39933bba5ab5ed5db6a7e
Eric Nguyen <eric@flux.io>
2016-03-31 01:03:49 -0700

    Initial commit. Blocks passing tests.

---
+++ src/bitmaskToCoordinates.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Turns a 2-dimensional bitmask (1's and 0's) and turns it into an array of
+ * [x,y,z] unit coordinates, a coordinate for every position where the value is
+ * 1.
+ *
+ * @param {Array.<Array.<Number>>} matrixMask
+ *     2-dimensional bitmask (1's and 0's).
+ *
+ * @return {Array.<Array.<Number>>} Array of [x,y,z] unit coordinates.
+ */
+function run(matrixMask) {
+	var coordinates = [];
+	for (var row = 0; row < matrixMask.length; row += 1) {
+		for (var col = 0; col < matrixMask[row].length; col += 1) {
+      if (matrixMask[row][col] === 1) {
+        coordinates.push([col, row, 0]);
+      }
+		}
+	}
+
+	return {
+		coordinates: coordinates
+	};
+}
+
+module.exports = {
+    run: run
+};
